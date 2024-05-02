9ae468ad02ad5ce195b013c6e7f4f223911dec54
James Bawa <jbawa032@gmail.com>
2024-05-02 02:56:39 -0700

    changed folder name

---
+++ basics/print_tebahpla.cs
@@ -0,0 +1,17 @@
+// printing alphabets in reverse
+
+
+using System;
+
+class Program
+{
+	static void Main()
+	{
+		char alpha = 'z';
+		while(alpha >= 'a')
+		{
+			Console.WriteLine(alpha);
+			alpha--;
+		}
+	}
+}
