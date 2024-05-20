87a778b80e708d8644bccf3d753ee9142f9bfb5d
James Bawa <jbawa032@gmail.com>
2024-05-20 08:29:39 +0000

    new file

---
+++ Methods/print_name.cs
@@ -0,0 +1,16 @@
+using System;
+
+
+class Program
+{
+
+        static void print_name(string name)
+        {
+                Console.WriteLine("Hello " + name);
+        }
+
+        static void Main()
+        {
+                print_name("James");
+        }
+}
